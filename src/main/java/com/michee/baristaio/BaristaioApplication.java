4f9ed124903e8ad72b61573abfa785bfd1afe7f8
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-07-18 15:33:30 +0200

    Initial commit

---
+++ src/main/java/com/michee/baristaio/BaristaioApplication.java
@@ -0,0 +1,13 @@
+package com.michee.baristaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaristaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaristaioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michee/baristaio/BaristaioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michee.baristaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaristaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
